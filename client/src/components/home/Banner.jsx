3663c928d27b8d3a103d9a770559d6c742960e39
j <joao.eudes13jem@gmail.com>
2025-12-10 07:34:22 -0300

    add components to /components/home

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -3,9 +3,9 @@ const Banner = () => {
         <div className="w-full py-2.5 font-medium text-sm text-green-800 text-center bg-gradient-to-r from-[#ABFF7E] to-[#FDFEFF]">
             <p>
                 <span className="px-3 py-1 rounded-lg text-white bg-green-600 mr-2">
-                    New
+                    Novo
                 </span>
-                AI Website Builder
+                Criação de Currículos com IA
             </p>
         </div>
     )
