81dda2446975a73f3e81342680956fe4b14736fc
notebook da lesli <joao.eudes13jem@gmail.com>
2026-01-04 15:02:32 -0300

    add +personal infos values to PersonaInfoForm.jsx

--- client/src/components/PersonalInfoForm.jsx
+++ client/src/components/PersonalInfoForm.jsx
@@ -1,75 +1,177 @@
-import { User } from 'lucide-react'
+import {
+    User,
+    Mail,
+    Phone,
+    MapPin,
+    BriefcaseBusiness,
+    Linkedin,
+    Globe,
+} from "lucide-react"
 
 const PersonalInfoForm = ({
-   data,
-   onChange,
-   removeBackground,
-   setRemoveBackground,
+    data,
+    onChange,
+    removeBackground,
+    setRemoveBackground,
 }) => {
-   const handleChange = (field, value) => {
-      onChange({ ...data, [field]: value })
-   }
+    const handleChange = (field, value) => {
+        onChange({ ...data, [field]: value })
+    }
 
-   return (
-      <div>
-         <h3
-            className="
+    const fields = [
+        {
+            key: "full_name",
+            label: "Nome Completo",
+            icon: User,
+            type: "text",
+            required: true,
+        },
+        {
+            key: "email",
+            label: "Endereço de Email",
+            icon: Mail,
+            type: "email",
+            required: true,
+        },
+        { key: "phone", label: "Número de Telefone", icon: Phone, type: "tel" },
+        { key: "location", label: "Localização", icon: MapPin, type: "text" },
+        {
+            key: "profession",
+            label: "Profissão",
+            icon: BriefcaseBusiness,
+            type: "text",
+        },
+        {
+            key: "linkedin",
+            label: "Perfil do Linkedin",
+            icon: Linkedin,
+            type: "url",
+        },
+        { key: "website", label: "Website Pessoal", icon: Globe, type: "url" },
+    ]
+
+    return (
+        <div>
+            <h3
+                className="
             text-lg font-semibold text-gray-900
            "
-         >
-            Informações Pessoais
-         </h3>
-         <p
-            className="
+            >
+                Informações Pessoais
+            </h3>
+            <p
+                className="
             text-sm text-gray-600
+            mb-3
             "
-         >
-            Comece com suas informações básicas
-         </p>
+            >
+                Comece com suas informações básicas
+            </p>
 
-         <div className="flex items-center gap-2">
-            <label>
-               {data.image ? (
-                  <img
-                     src={
-                        typeof data.image === 'string'
-                           ? data.image
-                           : URL.createObjectURL()
-                     }
-                     alt="user-image"
-                     className="
+            <div className="flex items-center gap-2">
+                <label>
+                    {data.image ? (
+                        <img
+                            src={
+                                typeof data.image === "string"
+                                    ? data.image
+                                    : URL.createObjectURL(data.image)
+                            }
+                            alt="user-image"
+                            className="
                      w-16 h-16 rounded-full object-cover
-                     mt-5 ring ring-slate-300 hover:opacity-80
+                     mt-5 mb-2 ring ring-slate-300 hover:opacity-80
                   "
-                  />
-               ) : (
-                  <div>
-                     <User className="size-10 p-2.5 border rounded-full" />
-                     Carregar imagem de usuário
-                  </div>
-               )}
+                        />
+                    ) : (
+                        <div className="flex items-center gap-2 mb-2">
+                            <User className="size-10 p-2.5 border rounded-full" />
+                            Carregar imagem de usuário
+                        </div>
+                    )}
 
-               <input
-                  onChange={(e) => handleChange('image', e.target.files[0])}
-                  type="file"
-                  accept="image/jpeg, image/png"
-               />
-            </label>
+                    <input
+                        className="mt-3"
+                        onChange={(e) =>
+                            handleChange("image", e.target.files[0])
+                        }
+                        type="file"
+                        accept="image/jpeg, image/png"
+                    />
+                </label>
 
-            {typeof data.image === 'object' && (
-               <div
-                  className="
-                     flex flex-col gap-1
-                     pl-4 text-sm
-                  "
-               >
-                  <p>Remover Fundo</p>
-                  <>Aqui</>
-               </div>
-            )}
-         </div>
-      </div>
-   )
+                {typeof data.image === "object" && (
+                    <div
+                        className="
+                        flex flex-col gap-1
+                        pl-4 text-sm
+                        "
+                    >
+                        <p>Remover Fundo</p>
+                        <label
+                            className="
+                            relative inline-flex items-center
+                            cursor-pointer text-gray-900 gap-3
+                            "
+                        >
+                            <input
+                                type="checkbox"
+                                className="sr-only peer"
+                                onChange={() =>
+                                    setRemoveBackground((prev) => !prev)
+                                }
+                                checked={removeBackground}
+                            />
+
+                            <div
+                                className="
+                                w-9 h-5 bg-slate-300 
+                                rounded-full peer
+                                peer-checked:bg-green-600
+                                transition-colors duration-200
+                                "
+                            ></div>
+
+                            <span
+                                className="dot absolute left-1 top-1 w-3 h-3
+                                bg-white rounded-full transition-transform
+                                duration-200 ease-in-out peer-checked:translate-x-4
+                                "
+                            ></span>
+                        </label>
+                    </div>
+                )}
+            </div>
+
+            {fields.map((field) => {
+                const Icon = field.icon
+
+                return (
+                    <div key={field.key} className="space-y-1 mt-5">
+                        <label
+                            className=" 
+                            flex items-center
+                            gap-2 text-sm
+                            font-medium 
+                            text-gray-600
+                            "
+                        >
+                            <Icon className="size-4" />
+                            {field.label}
+                            {field.required && (
+                                <span className="text-red-500">*</span>
+                            )}
+                        </label>
+                        <input 
+                           type={field.type} 
+                           value={data[field.key] || ""} 
+                           
+                        />
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default PersonalInfoForm
--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,91 +1,92 @@
-import { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useState, useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
 import {
-   ArrowLeftIcon,
-   User,
-   FileText,
-   Briefcase,
-   GraduationCap,
-   FolderIcon,
-   Sparkles,
-   ChevronLeft,
-   ChevronRight,
-} from 'lucide-react'
+    ArrowLeftIcon,
+    User,
+    FileText,
+    Briefcase,
+    GraduationCap,
+    FolderIcon,
+    Sparkles,
+    ChevronLeft,
+    ChevronRight,
+} from "lucide-react"
+import PersonalInfoForm from "../components/PersonalInfoForm.jsx"
 
 const ResumeBuider = () => {
-   const { resumeId } = useParams()
+    const { resumeId } = useParams()
 
-   const [resumeData, setResumeData] = useState({
-      _id: '',
-      title: '',
-      personal_info: {},
-      professional_summary: '',
-      experience: [],
-      education: [],
-      project: [],
-      skills: [],
-      tamplate: 'classic',
-      accent_color: '#3B82F6',
-      public: false,
-   })
+    const [resumeData, setResumeData] = useState({
+        _id: "",
+        title: "",
+        personal_info: {},
+        professional_summary: "",
+        experience: [],
+        education: [],
+        project: [],
+        skills: [],
+        tamplate: "classic",
+        accent_color: "#3B82F6",
+        public: false,
+    })
 
-   const dummyResumeData = [
-      {
-         _id: '1',
-         title: 'Frontend Developer Resume',
-         name: 'João',
-         skills: ['React', 'JS'],
-      },
-      {
-         _id: '2',
-         title: 'Backend Developer Resume',
-         name: 'Maria',
-         skills: ['Node', 'SQL'],
-      },
-   ]
+    const dummyResumeData = [
+        {
+            _id: "1",
+            title: "Frontend Developer Resume",
+            name: "João",
+            skills: ["React", "JS"],
+        },
+        {
+            _id: "2",
+            title: "Backend Developer Resume",
+            name: "Maria",
+            skills: ["Node", "SQL"],
+        },
+    ]
 
-   const [activeSectionIndex, setActiveSectionIndex] = useState(2)
-   const [removeBackground, setRemoveBackground] = useState(false)
+    const [activeSectionIndex, setActiveSectionIndex] = useState(0)
+    const [removeBackground, setRemoveBackground] = useState(false)
 
-   const sections = [
-      { id: 'personal', name: 'Personal Info', icon: User },
-      { id: 'summay', name: 'Summary', icon: FileText },
-      { id: 'experience', name: 'Experience', icon: Briefcase },
-      { id: 'education', name: 'Education', icon: GraduationCap },
-      { id: 'projects', name: 'Projects', icon: FolderIcon },
-      { id: 'skills', name: 'Skills', icon: Sparkles },
-   ]
+    const sections = [
+        { id: "personal", name: "Personal Info", icon: User },
+        { id: "summay", name: "Summary", icon: FileText },
+        { id: "experience", name: "Experience", icon: Briefcase },
+        { id: "education", name: "Education", icon: GraduationCap },
+        { id: "projects", name: "Projects", icon: FolderIcon },
+        { id: "skills", name: "Skills", icon: Sparkles },
+    ]
 
-   const activeSection = sections[activeSectionIndex]
+    const activeSection = sections[activeSectionIndex]
 
-   useEffect(() => {
-      const loadExistingResume = () => {
-         const resume = dummyResumeData.find(
-            (resume) => resume._id === resumeId
-         )
+    useEffect(() => {
+        const loadExistingResume = () => {
+            const resume = dummyResumeData.find(
+                (resume) => resume._id === resumeId
+            )
 
-         if (resume) {
-            setResumeData(resume)
-            document.title = resume.title
-         }
-      }
+            if (resume) {
+                setResumeData(resume)
+                document.title = resume.title
+            }
+        }
 
-      loadExistingResume()
-   }, [])
+        loadExistingResume()
+    }, [])
 
-   return (
-      <div>
-         <div
-            className="
+    return (
+        <div>
+            <div
+                className="
           max-w-7xl
           mx-auto
           px-4
           py-4
       "
-         >
-            <Link
-               to={'/app'}
-               className="
+            >
+                <Link
+                    to={"/app"}
+                    className="
                flex
                gap-2
                items-center
@@ -93,25 +94,25 @@ const ResumeBuider = () => {
                hover:text-slate-700
                transition-all
             "
-            >
-               <ArrowLeftIcon className="size-4" /> Voltar
-            </Link>
-         </div>
+                >
+                    <ArrowLeftIcon className="size-4" /> Voltar
+                </Link>
+            </div>
 
-         <div className="max-w-7xl mx-auto px-4 pb-8">
-            <div className="grid lg:grid-cols-12 gap-8">
-               {/* Left Panel - Form*/}
-               <div
-                  className="
+            <div className="max-w-7xl mx-auto px-4 pb-8">
+                <div className="grid lg:grid-cols-12 gap-8">
+                    {/* Left Panel - Form*/}
+                    <div
+                        className="
                         relative
                         lg:col-span-5
                         rounded-lg
                         overflow-hidden
                         
                     "
-               >
-                  <div
-                     className="
+                    >
+                        <div
+                            className="
                             bg-white
                             rounded-lg
                             shadow-sm
@@ -120,20 +121,20 @@ const ResumeBuider = () => {
                             p-6
                             pt-1
                        "
-                  >
-                     {/* progress bar using activeSectionIndex */}
+                        >
+                            {/* progress bar using activeSectionIndex */}
 
-                     <hr
-                        className="
+                            <hr
+                                className="
                               absolute 
                               top-0
                               left-0
                               right-0
                               border-2
                           "
-                     />
-                     <hr
-                        className="
+                            />
+                            <hr
+                                className="
                               absolute
                               top-0
                               left-0
@@ -143,79 +144,101 @@ const ResumeBuider = () => {
                               transition-all
                               duration-1000
                               "
-                        style={{
-                           width: `${(activeSectionIndex * 100) / (sections.length - 1)}%`,
-                        }}
-                     />
+                                style={{
+                                    width: `${(activeSectionIndex * 100) / (sections.length - 1)}%`,
+                                }}
+                            />
 
-                     {/* section navigation */}
+                            {/* section navigation */}
 
-                     <div
-                        className="
+                            <div
+                                className="
                           flex justify-between
                           items-center mb-6
                           border-b py-1
                           border-gray-300
                        "
-                     >
-                        <div></div>
-                        <div className="flex items-center">
-                           {activeSectionIndex !== 0 && (
-                              <button
-                                 onClick={() =>
-                                    setActiveSectionIndex((prevIndex) =>
-                                       Math.max(prevIndex - 1, 0)
-                                    )
-                                 }
-                                 className="
+                            >
+                                <div></div>
+                                <div className="flex items-center">
+                                    {activeSectionIndex !== 0 && (
+                                        <button
+                                            onClick={() =>
+                                                setActiveSectionIndex(
+                                                    (prevIndex) =>
+                                                        Math.max(
+                                                            prevIndex - 1,
+                                                            0
+                                                        )
+                                                )
+                                            }
+                                            className="
                                    flex items-center gap-1
                                    p-3 rounded-lg text-sm
                                    font-medium text-gray-600
                                    hover:bg-gray-50 transition-all
                                 "
-                                 disabled={activeSectionIndex === 0}
-                              >
-                                 <ChevronLeft className="size-4" /> Anterior
-                              </button>
-                           )}
+                                            disabled={activeSectionIndex === 0}
+                                        >
+                                            <ChevronLeft className="size-4" />{" "}
+                                            Anterior
+                                        </button>
+                                    )}
 
-                           <button
-                              onClick={() =>
-                                 setActiveSectionIndex((prevIndex) =>
-                                    Math.min(prevIndex + 1, sections.length - 1)
-                                 )
-                              }
-                              className={`
+                                    <button
+                                        onClick={() =>
+                                            setActiveSectionIndex((prevIndex) =>
+                                                Math.min(
+                                                    prevIndex + 1,
+                                                    sections.length - 1
+                                                )
+                                            )
+                                        }
+                                        className={`
                                     flex items-center gap-1
                                     p-1 rounded-lg text-sm
                                     font-medium text-gray-600
                                     hover:bg-gray-50 transition-all
-                                    ${activeSectionIndex === sections.length - 1 && 'opacity-50'}
+                                    ${activeSectionIndex === sections.length - 1 && "opacity-50"}
                                  `}
-                              disabled={
-                                 activeSectionIndex === sections.length - 1
-                              }
-                           >
-                              Proximo <ChevronRight className="size-4" />
-                           </button>
-                        </div>
-                     </div>
-              
-                     {/* Form Content */}
-                     <div className="space-y-6">
-                        {activeSection.id === "personal" && (
-                           <div></div>
-                        )}
-                     </div>
-                  </div>
-               </div>
+                                        disabled={
+                                            activeSectionIndex ===
+                                            sections.length - 1
+                                        }
+                                    >
+                                        Proximo{" "}
+                                        <ChevronRight className="size-4" />
+                                    </button>
+                                </div>
+                            </div>
 
-               {/* Right Panel - Preview*/}
-               <div></div>
+                            {/* Form Content */}
+                            <div className="space-y-6">
+                                {activeSection.id === "personal" && (
+                                    <PersonalInfoForm
+                                        data={resumeData.personal_info}
+                                        onChange={(data) =>
+                                            setResumeData((prev) => ({
+                                                ...prev,
+                                                personal_info: data,
+                                            }))
+                                        }
+                                        removeBackground={removeBackground}
+                                        setRemoveBackground={
+                                            setRemoveBackground
+                                        }
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Right Panel - Preview*/}
+                    <div></div>
+                </div>
             </div>
-         </div>
-      </div>
-   )
+        </div>
+    )
 }
 
 export default ResumeBuider
