18fabb740d68897e0369c00734f75e47ab02a166
j <joao.eudes13jem@gmail.com>
2026-01-09 10:31:52 -0300

    add basics templates infos on /components/TemplateSelector.jsx

--- client/src/components/TemplateSelector.jsx
+++ client/src/components/TemplateSelector.jsx
@@ -1,9 +1,36 @@
-const TemplateSelector = () => {
-   return (
-     <div>
+import { useState } from 'react'
 
-     </div>
-   )
+const TemplateSelector = ({ selectedTemplate, onChange }) => {
+   const [isOpen, setIsOpen] = useState(false)
+
+   const templates = [
+      {
+         id: 'classic',
+         name: 'Classic',
+         preview:
+            'Um currículo em formato tradicional, com tipografia profissional e formatos padrões.',
+      },
+      {
+         id: 'modern',
+         name: 'Modern',
+         preview:
+            'Currículo feito com uso estratégico no uso de cores e escolha moderna de fontes.',
+      },
+      {
+         id: 'minimal-image',
+         name: 'Minimal Image',
+         preview:
+            'Design sofisticado com uma pequena foto e tipografia minimalista.',
+      },
+      {
+         id: 'minimal',
+         name: 'Minimal',
+         preview:
+            'Design minimalista focado em exibir suas informações de forma direta e simples.',
+      },
+   ]
+
+   return <div></div>
 }
 
 export default TemplateSelector
