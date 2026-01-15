912429c45f8cfc28fcde4ec1ebef65ca6dc4eb8d
j <joao.eudes13jem@gmail.com>
2026-01-15 14:47:17 -0300

    starting to code the component ProfessionalSummaryForm.jsx

--- client/src/components/ProfessionalSummaryForm.jsx
+++ client/src/components/ProfessionalSummaryForm.jsx
@@ -1,9 +1,14 @@
-  const ProfessionalSummaryForm = () => {
+const ProfessionalSummaryForm = ({ data, onChange, setResumeData }) => {
    return (
-     <div>
-
-     </div>
+      <div className="space-y-4">
+         <div className="flex items-center justify-between">
+            <div>
+               <h3>Resumo Profissional</h3>
+            </div>
+            <button></button>
+         </div>
+      </div>
    )
 }
 
-  export default ProfessionalSummaryForm
+export default ProfessionalSummaryForm
