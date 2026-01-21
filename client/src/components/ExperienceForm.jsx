88e78575b91a8fce8e39a96b6926e7142ddb5b2a
notebook da lesli <joao.eudes13jem@gmail.com>
2026-01-20 22:19:45 -0300

    still editing ExperienceForm component

--- client/src/components/ExperienceForm.jsx
+++ client/src/components/ExperienceForm.jsx
@@ -1,4 +1,4 @@
-import { Plus } from 'lucide-react'
+import { Plus, Briefcase } from 'lucide-react'
 
 const ExperienceForm = ({ data, onChange }) => {
    const addExperience = () => {
@@ -61,12 +61,57 @@ const ExperienceForm = ({ data, onChange }) => {
              <div className="text-center py-8 text-gray-500">
                 <Briefcase className="w-12 h-12 mx-auto mb-3 text-gray-300"/>
                 <p>Sem experiência de trabalho ainda</p>
-                <p classN
+                <p className="text-sm">Clique "Adicione sua experiência" para começar.</p>
              </div>
          ) : (
-             <div>
+             <div className="space-y-4">
+                {data.map((experience, index) => (
+                   <div
+                      className="
+                         p-4 border border-gray-200
+                         rounded-lg space-y-3
+                      "
+                   > 
+                     <div className="flex justify-between items-start">
+                         <h4>Experiência #(index + 1)</h4>
+                         <button 
+                           onClick={() => removeExperience(index)}
+                           className="text-red-500 hover:text-red-700 transition-colors"
+                         >
+                             <Trash2 className="size-4"/> 
+                         </button>
+                     </div> 
+
+                     <div className="grid md:grid-cols-2 gap-3">
+                        <input 
+                           value={experience.company || ""}
+                           onChange={(e) => updateExperience(index, "company", e.target.value)}
+                           type="text"
+                           placeholder="Nome da Empresa"
+                           className="px-3 py-2 text-sm rounded-lg"
+                        /> 
+
+                         <input 
+                           value={experience.position || ""}
+                           onChange={(e) => updateExperience(index, "position", e.target.value)}
+                           type="text"
+                           placeholder="Nome da Função de Traabalho"
+                           className="px-3 py-2 text-sm rounded-lg"
+                        /> 
+
+                        <input 
+                           value={experience.start_date || ""}
+                           onChange={(e) => updateExperience(index, "start_date", e.target.value)}
+                           type="mounth"
+                           className="px-3 py-2 text-sm rounded-lg"
+                        /> 
+                     </div>
+                   </div>
+                ))} 
              <div>
          )}
       </div>
    )
 }
+
+export default ExperienceForm
