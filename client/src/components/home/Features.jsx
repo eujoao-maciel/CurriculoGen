7d6f03e7717c7091ddc3e4e02fbbea569219ce1d
notebook da lesli <joao.eudes13jem@gmail.com>
2026-02-13 13:59:32 -0300

    fix(feature): fix feature layout section

--- client/src/components/home/Features.jsx
+++ client/src/components/home/Features.jsx
@@ -1,177 +1,178 @@
-import { FileText, LayoutTemplate, Download } from 'lucide-react'
-import Title from './Title.jsx'
+import { FileText, LayoutTemplate, Download } from "lucide-react"
+import Title from "./Title.jsx"
 
 const Features = () => {
-   return (
-      <div
-         id="features"
-         className="flex flex-col items-center my-10 scroll-mt-12 bg-gradient-to-b from-gray-600 via-gray-800 to-gray-600"
-      >
-         <Title
-            title="Construa seu currículo"
-            description="Nosso processo simplificado ajuda você a criar um currículo profissional em minutos."
-         />
+    return (
+        <div
+            id="features"
+            className="flex flex-col items-center my-10 scroll-mt-12 bg-gradient-to-b from-gray-600 via-gray-800 to-gray-600"
+        >
+            <Title
+                title="Construa seu currículo"
+                description="Nosso processo simplificado ajuda você a criar um currículo profissional em minutos."
+            />
 
-         {/* Container principal */}
-         <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-6xl gap-12 mt-10 px-4">
-            {/* Imagem */}
-            <div className="w-full md:w-1/2 flex justify-center">
-               <img
-                  className="w-full max-w-2xl h-auto object-contain rounded-2xl transition-all duration-500"
-                  src="/imgs/imagemilustrativafeature.jpg"
-                  alt="Interface do gerador de currículos"
-                  style={{
-                     maskImage:
-                        'radial-gradient(circle, black 60%, transparent 100%)',
-                     WebkitMaskImage:
-                        'radial-gradient(circle, black 60%, transparent 100%)',
-                  }}
-               />
-            </div>
+            {/* Container principal */}
+            <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-6xl gap-12 mt-10 px-4">
+                {/* Imagem */}
+                <div className="w-full md:w-1/2 flex justify-center">
+                    <img
+                        className="p-4 w-full max-w-2xl h-auto object-contain rounded-2xl transition-all duration-500"
+                        src="/imgs/imagemilustrativafeature.jpg"
+                        alt="Interface do gerador de currículos"
+                        style={{
+                            maskImage:
+                                "radial-gradient(circle, black 60%, transparent 100%)",
+                            WebkitMaskImage:
+                                "radial-gradient(circle, black 60%, transparent 100%)",
+                        }}
+                    />
+                </div>
 
-            {/* Features */}
-            <div className="w-full md:w-1/2 space-y-6">
-               {/* Feature 1 */}
-               <div className="flex justify-center max-w-md mx-auto group cursor-pointer">
-                  <div
-                     className="
+                {/* Features */}
+                <div className="w-full md:w-1/2 space-y-6">
+                    {/* Feature 1 */}
+                    <div className="flex justify-center max-w-md mx-auto group cursor-pointer">
+                        <div
+                            className="
                                 p-6 flex gap-4 rounded-xl 
-                                group-hover:bg-red-200 group-hover:border-red-400
-                                group-active:bg-red-200 group-active:border-red-400
-                                transition-colors
+                                group-hover:bg-sky-100 group-hover:border-sky-300
+                                group-active:bg-sky-100 group-active:border-sky-300
+                                transition-colors 
                             "
-                  >
-                     <FileText
-                        className="
-                                    size-6 text-red-800
-                                    group-hover:text-red-900
-                                    group-active:text-red-900
+                        >
+                            <FileText
+                                className="
+                                    size-6 text-slate-200
+                                    group-hover:text-sky-700
+                                    group-active:text-sky-700
                                     transition-colors
                                 "
-                     />
+                            />
 
-                     <div className="space-y-2">
-                        <h3
-                           className="
+                            <div className="space-y-2">
+                                <h3
+                                    className="
                                         text-base font-semibold text-slate-200
-                                        group-hover:text-red-900
-                                        group-active:text-red-900
+                                        group-hover:text-sky-900
+                                        group-active:text-sky-900
                                         transition-colors
                                     "
-                        >
-                           Preenchimento Guiado
-                        </h3>
+                                >
+                                    Preenchimento Guiado
+                                </h3>
 
-                        <p
-                           className="
+                                <p
+                                    className="
                                         text-sm text-slate-300
-                                        group-hover:text-red-800
-                                        group-active:text-red-800
+                                        group-hover:text-sky-800
+                                        group-active:text-sky-800
                                         transition-colors
                                     "
-                        >
-                           Informe seus dados passo a passo e deixe que a
-                           plataforma organize tudo da forma correta.
-                        </p>
-                     </div>
-                  </div>
-               </div>
+                                >
+                                    Informe seus dados passo a passo e deixe que
+                                    a plataforma organize tudo da forma correta.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
-               {/* Feature 2 */}
-               <div className="flex justify-center max-w-md mx-auto group cursor-pointer">
-                  <div
-                     className="
+                    {/* Feature 2 */}
+                    <div className="flex justify-center max-w-md mx-auto group cursor-pointer">
+                        <div
+                            className="
                                 p-6 flex gap-4 rounded-xl border border-transparent
-                                group-hover:bg-green-100 group-hover:border-green-300
-                                group-active:bg-green-100 group-active:border-green-300
+                                group-hover:bg-sky-100 group-hover:border-sky-300
+                                group-active:bg-sky-100 group-active:border-sky-300
                                 transition-colors
                             "
-                  >
-                     <LayoutTemplate
-                        className="
-                                    size-6 text-green-600
-                                    group-hover:text-green-700
-                                    group-active:text-green-700
+                        >
+                            <LayoutTemplate
+                                className="
+                                    size-6 text-slate-200
+                                    group-hover:text-sky-700
+                                    group-active:text-sky-700
                                     transition-colors
                                 "
-                     />
+                            />
 
-                     <div className="space-y-2">
-                        <h3
-                           className="
+                            <div className="space-y-2">
+                                <h3
+                                    className="
                                         text-base font-semibold text-slate-200
-                                        group-hover:text-green-900
-                                        group-active:text-green-900
+                                        group-hover:text-sky-900
+                                        group-active:text-sky-900
                                         transition-colors
                                     "
-                        >
-                           Modelos Profissionais
-                        </h3>
+                                >
+                                    Modelos Profissionais
+                                </h3>
 
-                        <p
-                           className="
+                                <p
+                                    className="
                                         text-sm text-slate-300
-                                        group-hover:text-green-800
-                                        group-active:text-green-800
+                                        group-hover:text-sky-800
+                                        group-active:text-sky-800
                                         transition-colors
                                     "
-                        >
-                           Escolha entre layouts modernos e bem estruturados,
-                           pensados para processos seletivos reais.
-                        </p>
-                     </div>
-                  </div>
-               </div>
+                                >
+                                    Escolha entre layouts modernos e bem
+                                    estruturados, pensados para processos
+                                    seletivos reais.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
-               {/* Feature 3 */}
-               <div className="flex justify-center max-w-md mx-auto group cursor-pointer">
-                  <div
-                     className="
+                    {/* Feature 3 */}
+                    <div className="flex justify-center max-w-md mx-auto group cursor-pointer">
+                        <div
+                            className="
                                 p-6 flex gap-4 rounded-xl border border-transparent
-                                group-hover:bg-purple-100 group-hover:border-purple-300
-                                group-active:bg-purple-100 group-active:border-purple-300
+                                group-hover:bg-sky-100 group-hover:border-sky-300
+                                group-active:bg-sky-100 group-active:border-sky-300
                                 transition-colors
                             "
-                  >
-                     <Download
-                        className="
-                                    size-6 text-orange-600
-                                    group-hover:text-purple-700
-                                    group-active:text-purple-700
+                        >
+                            <Download
+                                className="
+                                    size-6 text-slate-200
+                                    group-hover:text-sky-700
+                                    group-active:text-sky-700
                                     transition-colors
                                 "
-                     />
+                            />
 
-                     <div className="space-y-2">
-                        <h3
-                           className="
+                            <div className="space-y-2">
+                                <h3
+                                    className="
                                         text-base font-semibold text-slate-200
-                                        group-hover:text-purple-900
-                                        group-active:text-purple-900
+                                        group-hover:text-sky-900
+                                        group-active:text-sky-900
                                         transition-colors
                                     "
-                        >
-                           Exportação Fácil
-                        </h3>
+                                >
+                                    Exportação Fácil
+                                </h3>
 
-                        <p
-                           className="
+                                <p
+                                    className="
                                         text-sm text-slate-300
-                                        group-hover:text-purple-800
-                                        group-active:text-purple-800
+                                        group-hover:text-sky-800
+                                        group-active:text-sky-800
                                         transition-colors
                                     "
-                        >
-                           Gere seu currículo pronto para envio ou impressão,
-                           sem complicações.
-                        </p>
-                     </div>
-                  </div>
-               </div>
+                                >
+                                    Gere seu currículo pronto para envio ou
+                                    impressão, sem complicações.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   )
+        </div>
+    )
 }
 
 export default Features
--- client/src/components/home/Title.jsx
+++ client/src/components/home/Title.jsx
@@ -1,8 +1,8 @@
 const Title = ({ title, description }) => {
   return (
     <div className="text-center mt-6 text-slate-700">
-        <h2 className="text-3xl text-slate-200 sm:text-4x1 font-medium">{title}</h2>
-        <p className="max-sm max-w-2xl mt-4 text-slate-300">{description}</p>
+        <h2 className="text-3xl p-4 text-slate-200 sm:text-4x1 font-medium">{title}</h2>
+        <p className="max-sm max-w-2xl p-4 mt-4 text-slate-300">{description}</p>
     </div>
   )
 }
