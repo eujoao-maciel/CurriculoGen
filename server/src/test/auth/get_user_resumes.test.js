f25b4e7cb265406973d4340e404b806f7cd11b5a
j <joao.eudes13jem@gmail.com>
2026-03-03 01:54:19 -0300

    test: add integration tests for getUserResumes endpoint

--- server/src/test/auth/get_user_resumes.test.js
+++ server/src/test/auth/get_user_resumes.test.js
@@ -3,12 +3,13 @@ import { describe, expect, it } from 'vitest'
 import jwt from 'jsonwebtoken'
 import { app } from '../../app.js'
 import { User } from '../../models/User.js'
+import { Resume } from '../../models/Resume.js'
 import { useMongoMemoryServer } from '../setup/mongoMemoryServer.js'
 
 useMongoMemoryServer()
 
-describe('GET /users/data', () => {
-   it('should return 200 and user data successfully.', async () => {
+describe('GET /users/resumes', () => {
+   it('should return user resumes with status 200', async () => {
       const user = await User.create({
          name: 'name',
          email: 'name@email.com',
@@ -17,18 +18,27 @@ describe('GET /users/data', () => {
 
       const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
 
+      await Resume.create({
+         title: 'resume_1',
+         userId: user._id,
+      })
+
+      await Resume.create({
+         title: 'resume_2',
+         userId: user._id,
+      })
+
       const res = await request(app)
-         .get('/users/data')
+         .get('/users/resumes')
          .set('Authorization', `Bearer ${token}`)
 
       expect(res.status).toBe(200)
-      expect(res.body.user.email).toEqual('name@email.com')
+      expect(res.body.resumes).toHaveLength(2)
    })
 
-   it('should return 404 if user does not exists', async () => {
-      const res = await request(app).get('/users/data')
+   it('should return 401 if no token is provided', async () => {
+      const res = await request(app).get('/users/resumes')
 
       expect(res.status).toBe(401)
-      expect(res.body.error).toBe('Access denied. No token provider.')
    })
 })
