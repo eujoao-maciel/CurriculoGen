cba026ed82fc3e0008e7bfb7b89ab2bca24c53e5
notebook da lesli <joao.eudes13jem@gmail.com>
2026-03-07 16:58:08 -0300

    feat: add getResumeById endpoint

--- server/src/routes/resume.js
+++ server/src/routes/resume.js
@@ -1,23 +1,33 @@
-import { Router } from 'express'
-import { authMiddleware } from '../middlewares/authMiddleware.js'
-import { validateRequest } from '../middlewares/validateRequest.js'
-import { createResume, deleteResume } from '../controllers/resumeController.js'
+import { Router } from "express"
+import { authMiddleware } from "../middlewares/authMiddleware.js"
+import { validateRequest } from "../middlewares/validateRequest.js"
 import {
-   titleValidation,
-   idValidation,
-} from '../validators/resumeValidations.js'
+    createResume,
+    deleteResume,
+    getResumeById,
+} from "../controllers/resumeController.js"
+import {
+    titleValidation,
+    idValidation,
+} from "../validators/resumeValidations.js"
 
 export const resumeRoutes = Router()
 
 resumeRoutes.post(
-   '/create',
-   authMiddleware,
-   validateRequest(titleValidation),
-   createResume
+    "/create",
+    authMiddleware,
+    validateRequest(titleValidation),
+    createResume
 )
 resumeRoutes.delete(
-   '/delete/:resumeId',
-   authMiddleware,
-   validateRequest(idValidation, 'params'),
-   deleteResume
+    "/delete/:resumeId",
+    authMiddleware,
+    validateRequest(idValidation, "params"),
+    deleteResume
+)
+resumeRoutes.get(
+    "/resumes/:resumeId",
+    authMiddleware,
+    validateRequest(idValidation, "params"),
+    getResumeById
 )
